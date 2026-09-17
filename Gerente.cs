7451770442eb70c81a5731b396531fb000f94a15
iza0914 <izabelli.izidoro@edu.senai.br>
2026-09-17 09:34:14 -0300

    Polimorfismo

---
+++ Gerente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo
+{
+    public class Gerente : Funcionario
+    {
+        public override double CalcularBonusAnual()
+
+        {
+
+            return (Salario * 0.15) + 1000;
+
+        }
+    }
+}
+
