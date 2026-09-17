331c2c61055c3c1228ecb34352850ae417404aef
iza0914 <izabelli.izidoro@edu.senai.br>
2026-09-17 08:42:19 -0300

    Adicionar arquivos de projeto.

---
+++ Geometria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo
+{
+    public class Geometria
+    {
+        public double CalcularArea(double Lado)
+        {
+            return Lado * Lado;
+        }
+        public double CalcularArea(double BaseRetangulo, double AlturaRetangulo)
+        {
+            return BaseRetangulo * AlturaRetangulo;
+        }
+
+        public double CalcularArea(double Raio, bool IsCirculo)
+        {
+            return Math.PI * Raio * Raio;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Polimorfismo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Geometria geometria = new Geometria();
+            double AreaQuadrado = geometria.CalcularArea(6);
+            double AreaRetangulo = geometria.CalcularArea(70, 7);
+            double AreaCirculo = geometria.CalcularArea(8, true);
+
+            Console.WriteLine($"A área do  quadrado: {AreaQuadrado:F2}");
+            Console.WriteLine($"A área do  retângulo: {AreaRetangulo:F2}");
+            Console.WriteLine($"A área do  circulo: {AreaCirculo:F2}");
+        }
+    }
+}
